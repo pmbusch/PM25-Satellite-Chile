b8a9e744960fecee857a286c1d3158309fe7e8de
Pablo <pmbusch@uc.cl>
2023-02-07 12:09:21 -0800

    Add initial PM2.5 data year and monthly

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # PM25-Satelite-Chile
-Analysis on PM2.5 data on Chile.
+Analysis on PM2.5 data on Chile. 
+Project developed for the course "STA250 - Data Science for International Development", UC Davis - Winter 2023.
+
+
+
+# Data Sources
+
+* **Air pollution**: Satellite data . PM2.5 estimates globally at a 0.01°x0.01° (0.01°~1.11km) resolution. Data available at yearly and monthly level. Data comes from [Van Donkelaar, A., Martin, R. V., Brauer, M. & Boys, B. L. Use of satellite
+observations for long-term exposure assessment of global concentrations of
+fine particulate matter. Environ. Health Perspect. 123, 135–143 (2015)](https://sites.wustl.edu/acag/datasets/surface-pm2-5/)
+
+* **Air pollution Ground data**: [SINCA-National Air Quality Information System] (https://sinca.mma.gob.cl/)
+	* Hourly and daily monitor data of PM2.5 concentrations. Multiple monitor stations with data available across Chile used to validate the satellite data.
+	* Data is obtained through scripts developed from another [project] (https://github.com/pmbusch/Reportes-SINCA)
+
+
+
+
+
---
+++ Scripts/preprocess_monitorData.R
@@ -0,0 +1,59 @@
+## Script to pre-process monitor data for PM2.5
+## PBH
+## February 2023
+
+library(tidyverse)
+theme_set(theme_bw(16)+ 
+            theme(panel.grid.major = element_blank(),
+                  axis.title.y=element_text(angle=0,margin=margin(r=0))))
+
+
+# read data obtained through web scraping for the whole period-----
+df <- readRDS("Data/Data_Original/pm25_daily.rds")
+
+# Filters -----
+
+# only consider validated data
+df$tipo_dato %>% table()
+df <- df %>% filter(tipo_dato=="validados")
+
+# years, since 2015
+df$year %>% table()
+df <- df %>% filter(year>2014 & year!=2023)
+
+df <- df %>% filter(unidad!="s/i")
+
+# change names
+df <- df %>% rename(province=provincia,
+                    commune=comuna,
+                    longitude=longitud,latitude=latitud)
+
+
+# Yearly data
+df_year <- df %>% group_by(region,province,commune,site,longitude,latitude,year,pollutant) %>% 
+  summarise(value=mean(valor,na.rm=T),
+            n=n(), 
+            completeness=n/365*100)
+
+ggplot(df_year,aes(completeness))+stat_ecdf()
+
+# initial filter, more than 75% of days with data in a given year
+df_year <- df_year %>% filter(completeness>=75)
+
+# number of stations
+df_year$site %>% unique() %>% length() # monitors
+df_year$year %>% table()
+
+write.table(df_year,"Data/pm25_year.csv",sep=";")
+
+# Monthly data
+df_month <- df %>% group_by(region,province,commune,site,longitude,latitude,year,month,pollutant) %>% 
+  summarise(value=mean(valor,na.rm=T),
+            n=n(), 
+            completeness=n/31*100)
+ggplot(df_month,aes(completeness))+stat_ecdf()
+df_month <- df_month %>% filter(completeness>=75)
+
+write.table(df_month,"Data/pm25_month.csv",sep=";")
+
+# EoF
