a4f15f5aef1e9b03015b1039f4c0e561c62e6dd6
Pablo <pmbusch@uc.cl>
2023-03-02 12:00:00 -0800

    Add more GLM models at quarter and month level

--- Scripts/regression_model.R
+++ Scripts/regression_model.R
@@ -15,9 +15,14 @@ mortality <- read.delim("Data/mortality_data.csv")
 mortality <- mortality %>% rename(year=Year,quarter=Quarter)
 
 # 75+ years death data----
-death_75 <- read.delim("Data/chile_elderly_mortality_count_comuna_level_year_quarter.csv",sep=",")
-death_75 <- death_75 %>% rename(year=Year,quarter=Quarter,
+# death_75 <- read.delim("Data/chile_elderly_mortality_count_comuna_level_year_quarter.csv",sep=",")
+death_75 <- read.delim("Data/chile_elderly_mortality_count_comuna_level_year_month.csv",sep=",")
+death_75 <- death_75 %>% rename(year=Year,
+                                # quarter=Quarter,
+                                month=Month,
                                 codigo_comuna=CODIGO_COMUNA_RESIDENCIA)
+death_75 <- death_75 %>% group_by(codigo_comuna,year,month) %>%
+  summarise(Mortality_Count=sum(Mortality_Count,na.rm=T))
 
 # get population
 chilemapas::censo_2017_comunas$edad %>% unique()
@@ -34,6 +39,20 @@ pop_75 <- pop_75 %>% filter(pop75>50)
 
 
 death_75 <- death_75 %>% left_join(pop_75)
+
+# death_75$codigo_comuna <- as.character(death_75$codigo_comuna)
+# a <- death_75 %>% left_join(chilemapas::codigos_territoriales)
+# 
+# a %>% group_by(year,month,codigo_region) %>% 
+#   summarise(Mortality_Count=sum(Mortality_Count,na.rm=T),
+#             pop75=sum(pop75,na.rm=T)) %>% 
+#   mutate(a=paste0(year,month)) %>%
+#   mutate(mortality=Mortality_Count/pop75*1000) %>% 
+#   ggplot(aes(a,mortality,group=codigo_region))+
+#   theme(axis.text.x = element_text(angle=90))+
+#   geom_line()+
+#   facet_wrap(~codigo_region)
+
 death_75 <- death_75 %>% mutate(mortality=Mortality_Count/pop75*1000)
 
 # pm25 pollution data exposure
@@ -44,8 +63,7 @@ pm25_exp <- pm25 %>%
   mutate(quarter=ceiling(month/3) %>% as.integer()) %>% # quarters by months
   # mutate(codigo_region=as.factor(codigo_region)) %>% 
   mutate(pop_pm25=poblacion*pm25_Exposure) %>% 
-  # group_by(codigo_region,year,quarter) %>% 
-  group_by(codigo_region,codigo_provincia,codigo_comuna,year,quarter) %>% 
+  group_by(codigo_region,codigo_provincia,codigo_comuna,year,month) %>% 
   summarise(pop_pm25=sum(pop_pm25,na.rm=T),
             total_pop=sum(poblacion,na.rm = T)) %>% 
   ungroup() %>% 
@@ -70,31 +88,35 @@ df <- df %>%
   mutate(year=as.factor(year),
          region=as.factor(codigo_region),
          commune=as.factor(codigo_comuna),
-         quarter=as.factor(quarter))
+         month=as.factor(month)
+         # quarter=as.factor(quarter)
+         )
 
-write.table(df,"Data/panelData.csv",sep = ";",row.names = F)
+# write.table(df,"Data/panelData.csv",sep = ";",row.names = F)
 
 
 ## Correlation ---
 cor(df$mortality,df$pm25_exposure)
 cor(df$mortality,df$pm25_exposure,method = "spearman")
 
-df %>% group_by(quarter) %>% 
+df %>% group_by(month) %>% 
   summarise(cor(mortality,pm25_exposure))
 
 
 # Some figures -----
 df %>% 
-  mutate(a=paste0(year,quarter)) %>% 
+  mutate(a=paste0(year,month)) %>%
   ggplot(aes(a,mortality,group=commune))+
-  geom_line()
+  geom_line(alpha=.5)
 
-ggplot(df,aes(quarter,mortality))+
+ggplot(df,aes(month,mortality))+
   geom_boxplot()
 
 ggplot(df,aes(pm25_exposure,mortality,col=quarter))+
+  geom_point(alpha=.5, data=dplyr::select(df,-quarter),col="grey")+
   geom_point(alpha=.5)+
-  geom_smooth()+
+  geom_smooth(method="lm")+
+  geom_smooth(method="lm", data=dplyr::select(df,-quarter),col="grey")+
   facet_wrap(~quarter)
 
 
@@ -107,19 +129,87 @@ model_nb <- glm.nb(Mortality_Count ~ pm25Exp_10ug+year+region+
 
 summary(model_nb)
 nobs(model_nb)
+BIC(model_nb)
+coef(model_nb) %>% exp()
 confint(model_nb) %>% exp()
 autoplot(model_nb)
+plot(df$mortality,predict(model_nb,type="response"))
 
 # LM
 model_lm <- glm(mortality ~ pm25Exp_10ug+year+region, 
-                   data = df,
-                family = "gaussian",
-                   na.action=na.omit)
+                data = df,
+                family =  gaussian(link = "log"),
+                na.action=na.omit)
 
 summary(model_lm)
 nobs(model_lm)
-confint(model_lm)
+BIC(model_lm)
+coef(model_lm) %>% exp()
+confint(model_lm) %>% exp()
 autoplot(model_lm)
+
+## Gamma regression -----
+model_gamma <- glm(mortality ~ pm25Exp_10ug+year+region, 
+                data = df,
+                family =  Gamma(link = "log"),
+                na.action=na.omit)
+
+summary(model_gamma)
+nobs(model_gamma)
+BIC(model_gamma)
+coef(model_gamma) %>% exp()
+confint(model_gamma) %>% exp()
+autoplot(model_gamma)
+
+
+## Random Effects Region-----
+model_random <- glmer.nb(Mortality_Count ~ pm25Exp_10ug+year+(1 | region)+
+                     offset(log(pop75)), 
+                   data = df,
+                   na.action=na.omit)
+
+summary(model_random)
+nobs(model_random)
+BIC(model_random)
+fixef(model_random) %>% exp()
+confint(model_random, method="Wald") %>% exp()
+
+## Random Effects Comune-----
+model_random_com <- glmer.nb(Mortality_Count ~ pm25Exp_10ug+year+(1 | commune)+
+                           offset(log(pop75)), 
+                         data = df,
+                         na.action=na.omit)
+
+summary(model_random_com)
+nobs(model_random_com)
+BIC(model_random_com)
+fixef(model_random_com) %>% exp()
+confint(model_random_com, method="Wald") %>% exp()
+
+
+# Model with lags -----
+df <- df %>% 
+  mutate(pm25Exp_lag1=lag(pm25Exp_10ug,1),
+         pm25Exp_lag2=lag(pm25Exp_10ug,2),
+         pm25Exp_lag3=lag(pm25Exp_10ug,3),
+         pm25Exp_lag4=lag(pm25Exp_10ug,4))
+
+
+model_nb_lags <- glm.nb(Mortality_Count ~ pm25Exp_10ug+pm25Exp_lag1+
+                          pm25Exp_lag2+pm25Exp_lag3+pm25Exp_lag4+
+                          year+region+
+                     offset(log(pop75)), 
+                   data = df,
+                   na.action=na.omit)
+
+summary(model_nb_lags)
+nobs(model_nb_lags)
+BIC(model_nb_lags)
+coef(model_nb_lags) %>% exp()
+confint(model_nb_lags) %>% exp()
+autoplot(model_nb_lags)
+
+
 
 ## Panel estimators
 # https://rdrr.io/cran/pglm/man/pglm.html
