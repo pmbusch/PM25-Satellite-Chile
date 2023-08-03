2c71d265b4846e614928cbeb44d0274b2d4f2f9b
Pablo <pmbusch@uc.cl>
2023-08-03 12:07:32 -0700

    Fix time series colors

--- Scripts/Other_Analysis/Figure_TimeSeries.R
+++ Scripts/Other_Analysis/Figure_TimeSeries.R
@@ -9,18 +9,20 @@ theme_set(theme_bw(16)+ theme(panel.grid.major = element_blank()))
 source("Scripts/Functions.R",encoding="UTF-8")
 
 # Load Panel Data ----
-df <- read.delim("Data/panelData.csv",sep=";")
+df <- read.delim("Data/Panel Data/panelData.csv",sep=";")
 
 # Figure ------
 names(df)
 # create 3 plots
+library(RColorBrewer)
+brewer.pal(n=3,"Reds") # color same as map
 p2 <- df %>% 
   # filter(year>2015) %>%
   mutate(date=as.Date(paste(year,month,"01",sep="-"),"%Y-%m-%d")) %>% 
   mutate(count_month=as.numeric(year)*12+as.numeric(month)) %>% # order by month
   arrange(count_month) %>% arrange(codigo_comuna) %>% 
   ggplot(aes(date,pm25_exposure,group=commune))+
-  geom_line(alpha=.2,linewidth=.1,col="#800000")+ 
+  geom_line(alpha=.2,linewidth=.1,col="#DE2D26")+ 
   coord_cartesian(expand = F)+
   labs(x="",y=lab_pm25)+
   scale_x_date(date_breaks = "2 year",date_labels = "%Y")+
@@ -28,10 +30,12 @@ p2 <- df %>%
   theme_bw(8)+
   theme(panel.grid.major = element_blank())
 # p2
-p3 <- p2+aes(y=landTemp)+labs(y=lab_temp)+geom_line(alpha=.2,linewidth=.1,col="#000080")
+brewer.pal(n=3,"Blues") 
+p3 <- p2+aes(y=landTemp)+labs(y=lab_temp)+geom_line(alpha=.2,linewidth=.1,col="#3182BD")
 # p3
+brewer.pal(n=3,"Oranges") 
 p1 <- p2+aes(y=MR_all_cause)+labs(y=lab_mr)+
-  geom_line(alpha=.2,linewidth=.1,col="black")
+  geom_line(alpha=.2,linewidth=.1,col="#E6550D")
 # p1
 
 # Arrange in row format
@@ -60,13 +64,12 @@ ggsave(sprintf("Figures/Timeseries.png"),p,
 df$codigo_comuna %>% unique() %>% length() # 327
 
 
-# Just T and PM2.5
-
-p2 <- p2 +   ggtitle(expression("A: PM"[2.5]~"Exposure"))
-p3 <- p3 +   ggtitle("B: Land Temperature")
+# Just PM2.5 and MR
+p2 <- p2 +   ggtitle(expression("PM"[2.5]~"Exposure"))
+p1 <- p1 +   ggtitle("75+ Mortality Rate All-Cause")
 
 p <- grid.arrange(arrangeGrob(p2),
-                  arrangeGrob(p3),
+                  arrangeGrob(p1),
                   ncol=1)
 p
 # 
